93df5e8777617ede779fbcf5d9b3775ddbccb672
Shaqra University <ccitlms@gmail.com>
2022-03-06 11:32:57 +0300

    Final Commit to StoryBoard Main

--- viewControllerTwin/ViewController.swift
+++ viewControllerTwin/ViewController.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var x = 10
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("View Did Load Method Called!")
+        print("The value of x is : \(x)")
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View WillAppear Method Called!")
+
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View DidAppear Method Called!")
+
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("View DidDisappear Method Called!")
+
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("View WillDisappear Method Called!")
+
+    }
+    
 
 
 }
